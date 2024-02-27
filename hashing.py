0c955c29cf240d711f9c47d2275574331eb5bf24
Humza <139543666+humzak711@users.noreply.github.com>
2024-02-27 00:19:37 +0000

    Update hashing.py

--- hashing.py
+++ hashing.py
@@ -2,6 +2,12 @@ import hashlib
 
 # Function to hash data
 def hash_data(data: str) -> str:
-    ''' Hashes data in a way which is designed to be deterministic'''
+    ''' 
+    Hashes data in a way which is designed to be deterministic
+    
+    Uses SHA256 hashing algorithm to ensure secure hashing with 
+    the smallest chance of collision 
+    '''
+    
     hashed_data: str = hashlib.sha256(data.encode()).hexdigest()
     return hashed_data
